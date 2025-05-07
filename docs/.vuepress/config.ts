71b09f5ced7e657a7b20e79076d0545d79d5e933
kazumatu981 <kazumatu981@gmail.com>
2025-05-08 07:36:57 +0900

    まずは最初のひな形

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,60 @@
+import {viteBundler} from '@vuepress/bundler-vite'
+import {defaultTheme} from '@vuepress/theme-default'
+import {defineUserConfig} from 'vuepress'
+
+export default defineUserConfig({
+    bundler: viteBundler(),
+    theme: defaultTheme({
+        navbar:[
+            {
+                text: 'about',
+                link: '/about.html',
+            }
+        ],
+        sidebar: [
+            {
+                text: '目標をきめる',
+                prefix: '/contents/',
+                link: '/contents/'
+            },
+            {
+                text: '名前にこだわる',
+                prefix: '/contents/naming/',
+                link: '/contents/naming/',
+                children: [
+                    {
+                        text: '名は体を表す',
+                        link: '/contents/naming/name-means-function.html'
+                    },
+                    {
+                        text: '動詞 vs 名詞',
+                        link: '/contents/naming/verb-vs-noun.html'
+                    },
+                    {
+                        text: '大文字と小文字',
+                        link: '/contents/naming/capitalization.html'
+                    },
+                ]
+            },
+            {
+                text: 'メトリクスにこだわる',
+                prefix: '/contents/metrics/',
+                link: '/contents/metrics/'
+            } ,
+            {
+                text: '仕組みにこだわる',
+                prefix: '/contents/functionblock/',
+                link: '/contents/functionblock/'
+            },           
+            {
+                text: 'このドキュメントについて',
+                link: '/about.html'
+            },
+        ]
+    }),
+
+    lang: 'ja-JP',
+    title: 'よみやすいコード',
+    description: 'よみやすいコードを書くために私たちは何をすべきか',
+    dest: `./dist`
+})
---
+++ docs/README.md
@@ -0,0 +1,21 @@
+---
+home: true
+actions:
+    - text: 目標を決める
+      link: /contents/
+
+    - text: 名前にこだわる
+      link: /contents/naming/
+
+    - text: metricsにこだわる
+      link: /contents/metrics/
+
+    - text: 仕組みにこだわる
+      link: /contents/functionblock/
+
+    - text: このドキュメントについて
+      link: /about.html
+      type: 'secondary'
+
+footer: MIT Licensed | Copyright © 2025 Kazuyoshi Matsumoto
+---
