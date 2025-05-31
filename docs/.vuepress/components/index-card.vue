4960763ecdc2db9296a0d9603574767dd2b4ed89
kazumatu981 <kazumatu981@gmail.com>
2025-05-31 12:57:38 +0900

    index-card 完成

---
+++ docs/.vuepress/components/index-card.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+    icon: {
+        type: String,
+        required: true
+    },
+    label: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+$corner-radius: 0.5rem;
+
+$labelTextColor: var(--vp-c-white);
+$labelBackgroundColor: var(--vp-c-accent);
+$contentBackgroundColor: var( --vp-c-bg-alt);
+
+.index-card {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+
+    border-style: solid;
+    border-width: 1.5px;
+    border-radius: $corner-radius;
+    background-color: $contentBackgroundColor;
+    box-shadow:5px 5px 10px #b2b2b2;
+
+    &:hover {
+        background-color: var(--vp-c-bg-soft);
+        box-shadow:none;
+    }
+
+    
+    .index-card__label {
+        display: flex;
+        align-items: center;
+        font-size: 1.5rem;
+
+        border-top-left-radius: $corner-radius;
+        border-top-right-radius: $corner-radius;
+        padding:  0.5rem 1rem;
+
+        background-color: $labelBackgroundColor;
+        color: $labelTextColor;
+        i {
+            padding-right: 0.5rem;
+        }
+    }
+    
+    .index-card__content {
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+
+        border-bottom-left-radius: $corner-radius;
+        border-bottom-right-radius: $corner-radius;
+        padding:  0.5rem 1rem;
+
+        .index-card__content-content {
+            flex: 1;
+        }
+    }
+    .index-card__link {
+        display: flex;
+        margin-top: auto;
+        padding-left: 0.5rem;
+        padding-bottom: 0.5rem;
+        a {
+            color: var(--vp-c-accent);
+        }
+    }
+}
+
+
+</style>
+
+<template>
+    <div class="index-card">
+        <div class="index-card__label">
+            <i class="fa-solid" :class="icon"></i>
+            {{ label }}
+        </div>
+        <div class="index-card__content">
+            <div class="index-card__content-content">
+                <slot></slot>
+            </div>
+        </div>
+        <div class="index-card__link">
+            <a :href="link">
+                中身を見る
+                <i class="fa-solid fa-arrow-right"></i>
+            </a>
+        </div>
+    </div>
+</template>
