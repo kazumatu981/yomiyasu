fd67483dfbbe74dbbe78d0d49d11525769646407
kazumatu981 <kazumatu981@gmail.com>
2025-06-05 22:11:59 +0900

    delete.

--- docs/.vuepress/components/labeled-container-with-fa-icon.vue
+++ docs/.vuepress/components/labeled-container-with-fa-icon.vue
@@ -27,15 +27,6 @@ defineProps({
 $corner-radius: 0.5rem;
 $icon-size: 1.2rem;
 
-$labelTextColor: var(--vp-c-accent-text);
-$contentBackgroundColor: var( --vp-c-bg-alt);
-$containerRed: var(--accent2-color);
-$containerGreen: var(--accent1-color);
-$containerYellow: var(--accent4-color);
-$containerBlue: var(--accent3-color);
-$containerGray: gray;
-
-
 .labeled-container {
     display: flex;
     flex-direction: column;
